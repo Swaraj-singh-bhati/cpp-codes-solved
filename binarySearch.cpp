918110ac32439ae653b14c3d023f7746b961841d
Swaraj Singh Bhati <114081769+Swaraj-singh-bhati@users.noreply.github.com>
2022-12-21 12:59:21 +0530

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int BinarySearch(int arr[], int size, int key)
 {
-    int start = 0,index,s;
+    int start = 0,index,mid;
     int end = size-1;
     while( start<=end )
     {
-        s = (start+end)/2; 
+        mid = start + (end-start)/2; 
 
-        if(key == arr[s])
+        if(key == arr[mid])
         {
-            index = s;
+            index = mid;
             return index;
         }
-        if (key < arr[s])
+        if (key < arr[mid])
         {
-            end = s-1;
+            end = mid-1;
         }
         else
         {
-            start = s+1;
+            start = mid+1;
         }
         
     }
@@ -32,7 +32,7 @@ int main()
     int odd[5] = {2,5,8,11,45};int ans=0;
     int even[6] = {1,4,7,9,13,15};
     
-    ans = BinarySearch(odd,5,45);
+    ans = BinarySearch(odd,5,11);
     cout<<"The element 11 is present at index "<<ans<<endl;
     return 0;
 }
