27a094c58c4f110e50d637031af23585ef6ae747
Swaraj Singh Bhati <114081769+Swaraj-singh-bhati@users.noreply.github.com>
2022-12-29 14:54:00 +0530

    Create isPalindrome.cpp

---
+++ isPalindrome.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+bool checkSpecialChar(char s)
+{
+    if(s>='a' && s<='z' || s>='0' && s<='9')
+        return false;
+    else 
+        return true;
+}
+
+char toLowerCase(char s) 
+{
+    if(s>='A' && s<='Z') 
+    return s-'A'+'a';
+    else 
+    return s;
+}
+bool isPalindrome(string s) 
+{
+    int e=s.length()-1, st=0;
+    while(st<=e) 
+    {
+        char ch1=toLowerCase(s[st]), ch2=toLowerCase(s[e]);    
+        
+        if(checkSpecialChar(ch1)) 
+        {
+            st++; 
+            continue;
+        } 
+        else if(checkSpecialChar(ch2)) 
+        {
+            e--;
+            continue;
+        }  
+        else 
+            if(ch1==ch2)
+            {
+                st++;e--;            
+            }
+            else 
+            return false;
+    }
+    return true;    
+}
+};
