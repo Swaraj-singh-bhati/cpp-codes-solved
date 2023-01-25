0e5ae093ea27e611ba800fd09e76eba8015ff6dc
Swaraj Singh Bhati <114081769+Swaraj-singh-bhati@users.noreply.github.com>
2023-01-25 14:07:53 +0530

    Update firstAndLastPosition.cpp

--- firstAndLastPosition.cpp
+++ firstAndLastPosition.cpp
@@ -27,3 +27,64 @@ pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
     ans = make_pair(b-a+1,b);
     return ans;
 }
+
+//optimised
+
+
+#include <bits/stdc++.h>
+using namespace std; 
+int firstOcc(vector<int>& arr, int start, int end, int k, int index)
+    {
+        if (start > end) 
+        {
+          return index;
+        }
+        int mid = start + (end-start)/2; 
+        if(arr[mid] == k)
+        {
+            index = mid;
+            end = mid - 1;
+            return firstOcc(arr,start,end,k,index);
+        }
+        if (arr[mid]>k)
+        {
+            return firstOcc(arr,start,mid-1,k,index);
+        }
+        else
+        {
+            return firstOcc(arr,mid+1,end,k,index);
+        }
+    }
+
+int lastOcc(vector<int>& arr, int start, int end, int k, int index)
+    {
+        if (start > end) 
+        {
+          return index;
+        }
+        int mid = start + (end-start)/2; 
+        if(arr[mid] == k)
+        {
+            index = mid;
+            start = mid + 1;
+            return lastOcc(arr,start,end,k,index);
+        }
+        if (arr[mid]>k)
+        {
+            return lastOcc(arr,start,mid-1,k,index);
+        }
+        else
+        {
+            return lastOcc(arr,mid+1,end,k,index);
+        }
+}
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+    pair<int, int> ans;
+    int index = -1;
+    int start = 0, end = arr.size()-1;
+    ans.first= firstOcc(arr,start,end,k,index);
+    ans.second= lastOcc(arr,start,end,k,index);
+    return ans;
+}    
+    
