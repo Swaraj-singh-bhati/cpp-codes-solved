56f8b2a8927efd57859a2cc8407b813eb9b63839
Swaraj Singh Bhati <114081769+Swaraj-singh-bhati@users.noreply.github.com>
2023-05-31 15:14:01 +0530

    Create flatten_BST.cpp

---
+++ flatten_BST.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the Binary Tree node structure
+    
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+************************************************************/
+
+void inorder(TreeNode<int>* root, vector<int> &inorderVal)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    inorder(root->left, inorderVal);
+    inorderVal.push_back(root->data);
+    inorder(root->right, inorderVal);
+
+}
+
+TreeNode<int>* flatten(TreeNode<int>* root)
+{
+    vector<int> inorderVal;
+    inorder(root, inorderVal);
+    int n = inorderVal.size();
+    TreeNode<int>* newRoot = new TreeNode<int>(inorderVal[0]);
+    TreeNode<int>* curr = newRoot;
+    for(int i=1; i<n; i++)
+    {
+        TreeNode<int>* temp = new TreeNode<int>(inorderVal[i]);
+        curr->left = NULL;
+        curr->right = temp;
+        curr = temp;
+    }
+    curr->left = NULL;
+    curr->right = NULL;
+    return newRoot;
+
+}
