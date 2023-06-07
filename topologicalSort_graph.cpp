afc4225da038b104e1ec6e05982b6bea77423501
Swaraj Singh Bhati <114081769+Swaraj-singh-bhati@users.noreply.github.com>
2023-06-07 13:01:51 +0530

    Update topologicalSort_graph.cpp

--- topologicalSort_graph.cpp
+++ topologicalSort_graph.cpp
@@ -1,5 +1,6 @@
-#include <bits/stdc++.h> 
+//using DFS
 
+#include <bits/stdc++.h> 
 
 void dfsTopo(unordered_map<int,list<int>>&adj,vector<bool>&visited,stack<int>&s,int node)
 {
@@ -48,6 +49,61 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)
         s.pop();
     }
 
+
+    return ans;
+}
+
+
+//using Kahn's algorithm BFS
+
+#include <bits/stdc++.h> 
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  
+{
+    //create adj list
+    unordered_map<int, list<int> > adj;
+    for(int i=0; i<e; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adj[u].push_back(v);
+    }
+
+    //find all indegrees
+    vector<int> indegree(v);
+    for(auto i: adj)
+    {
+        for(auto j: i.second)
+        {
+            indegree[j]++;
+        }
+    }
+
+    //push nodes which have 0 indegree
+    queue<int> q;
+    for(int i=0; i<v; i++)
+    {
+        if(indegree[i] == 0)
+        q.push(i);
+    }
+
+    //do bfs
+    vector<int> ans;
+    while(!q.empty())
+    {
+        int front = q.front();
+        q.pop();
+
+        ans.push_back(front);
+
+        for(auto neighbour: adj[front])
+        {
+            indegree[neighbour]--;
+            if(indegree[neighbour] == 0)
+            {
+                q.push(neighbour);
+            }
+        }
+    }
 
     return ans;
 }
