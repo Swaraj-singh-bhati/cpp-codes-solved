19d5cea6d708d8f5958182096c7500032c2aa75a
Swaraj Singh Bhati <114081769+Swaraj-singh-bhati@users.noreply.github.com>
2023-06-07 13:13:11 +0530

    Update detectCycleInDirectedGraph.cpp

--- detectCycleInDirectedGraph.cpp
+++ detectCycleInDirectedGraph.cpp
@@ -57,3 +57,66 @@ int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges)
   }
   return false;
 }
+
+
+//Using Kahn's algo BFS
+
+#include<bits/stdc++.h>
+
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) 
+{
+    //create adj list
+    unordered_map<int, list<int> > adj;
+    for(int i=0; i<edges.size(); i++)
+    {
+      int u = edges[i].first - 1;
+      int v = edges[i].second - 1;
+
+      adj[u].push_back(v);
+    }
+
+    //find all indegrees
+    vector<int>indegree(n);
+    for(auto i: adj)
+    {
+      for(auto j: i.second)
+      {
+        indegree[j]++;
+      }
+    }
+
+    // push nodes having 0 indegree
+    queue<int> q;
+    for(int i=0; i<n; i++)
+    {
+      if(indegree[i] == 0)
+      {
+        q.push(i);
+      }
+    }
+
+    // do bfs
+    int cnt = 0;
+    while(!q.empty())
+    {
+      int front = q.front();
+      q.pop();
+
+      cnt++;
+
+      //neighbour indegree update
+      for(auto neighbour: adj[front])
+      {
+        indegree[neighbour]--;
+        if(indegree[neighbour] == 0)
+        {
+          q.push(neighbour);
+        }
+      }
+    }
+
+    if(cnt == n)
+      return false;
+    else
+      return true;
+}
