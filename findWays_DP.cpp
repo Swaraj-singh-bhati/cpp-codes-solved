809e57bd4eb783dcc4c2002d37e875b8b33005c3
Swaraj Singh Bhati <114081769+Swaraj-singh-bhati@users.noreply.github.com>
2023-06-13 14:52:22 +0530

    Create findWays_DP.cpp
    Question link ->
    https://www.codingninjas.com/codestudio/problems/number-of-ways_3755252?leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar

---
+++ findWays_DP.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+
+int solve(vector<int> &num, int tar)
+{
+
+    vector<int> dp(tar+1, 0);
+
+    dp[0] = 1;
+
+    //traversing from target 1 to tar
+    for(int i=1; i<=tar; i++)
+    {
+        //traversing on num vector
+        for (int j = 0; j < num.size(); j++) 
+        {
+          if (i - num[j] >= 0)
+            dp[i] += dp[i - num[j]];
+        }
+    }
+    return dp[tar];
+}
+
+int findWays(vector<int> &num, int tar)
+{
+    return solve(num, tar);
+}
