eaf9d5c5dfbe61a2e377d902557ef402dd34d8d2
Swaraj Singh Bhati <114081769+Swaraj-singh-bhati@users.noreply.github.com>
2023-06-20 14:32:08 +0530

    Update moveZeroes.cpp
    Question link ->
    https://leetcode.com/problems/move-zeroes/

--- moveZeroes.cpp
+++ moveZeroes.cpp
@@ -12,3 +12,25 @@ public:
         }
     }
 };
+
+//Another Approach
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        int count = 0, size = nums.size(), zeroes;
+        for(int i=0; i<size; i++)
+        {
+            if(nums[i]!= 0)
+            {
+                nums[count++] = nums[i];
+            }
+        }
+        zeroes = size - count;
+        for(int i=count; i<size; i++)
+        {
+            nums[i] = 0;
+        }
+    }
+};
