615b5ca2f85509053dd5e7b81eea7fc004ef2a16
Swaraj Singh Bhati <114081769+Swaraj-singh-bhati@users.noreply.github.com>
2023-06-20 14:15:25 +0530

    Update rotate.cpp
    Question Link ->
    https://leetcode.com/problems/rotate-array/

--- rotate.cpp
+++ rotate.cpp
@@ -21,3 +21,16 @@ public:
       
     }
 };
+
+//Another Way
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) 
+    {
+        k %= nums.size();//if k is greater than nums then we take this
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
