6572a7874e04b86c9ee51b7f2fd8f5c8c69cf546
Swaraj Singh Bhati <114081769+Swaraj-singh-bhati@users.noreply.github.com>
2023-06-22 16:14:36 +0530

    Update binarySearch.cpp
    Question Link ->
    https://leetcode.com/problems/binary-search/description/

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,38 +1,34 @@
-#include<iostream>
-using namespace std;
+class Solution {
+public:
 
-int BinarySearch(int arr[], int size, int key)
-{
-    int start = 0,index,mid;
-    int end = size-1;
-    while( start<=end )
+    int solve(vector<int>& nums, int target)
     {
-        mid = start + (end-start)/2; 
+        int start = 0;
+        int end = nums.size()-1;
+        int mid;
+        while(start<=end)
+        {
+            mid = start + (end - start) / 2;
 
-        if(key == arr[mid])
-        {
-            index = mid;
-            return index;
+            if(nums[mid] == target)
+                return mid;
+            
+            else if(nums[mid] < target)
+            {
+                start = mid + 1;
+            }
+
+            else
+            {
+               end = mid -1; 
+            }
         }
-        if (key < arr[mid])
-        {
-            end = mid-1;
-        }
-        else
-        {
-            start = mid+1;
-        }
-        
+
+        return -1;
     }
-    return -1;
-}
 
-int main()
-{
-    int odd[5] = {2,5,8,11,45};int ans=0;
-    int even[6] = {1,4,7,9,13,15};
-    
-    ans = BinarySearch(odd,5,11);
-    cout<<"The element 11 is present at index "<<ans<<endl;
-    return 0;
-}
+    int search(vector<int>& nums, int target) 
+    {
+        return solve(nums,target);    
+    }
+};
