3208e8ec323dcff36ee1b6efdf28879e6eca0112
Swaraj Singh Bhati <114081769+Swaraj-singh-bhati@users.noreply.github.com>
2023-07-01 14:24:58 +0530

    Update isBalanced.cpp

--- isBalanced.cpp
+++ isBalanced.cpp
@@ -1,91 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// Tree Node
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
-};
-
-// Utility function to create a new Tree Node
-Node* newNode(int val) {
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-
-    return temp;
-}
-
-
-// Function to Build Tree
-Node* buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N') return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;) ip.push_back(str);
-
-    // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size()) break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
 /* A binary tree node structure
 
 struct Node
@@ -140,27 +52,45 @@ class Solution
     }
     
 };
+//another solution
 
-//{ Driver Code Starts.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+    
+    int solve(TreeNode* root)
+    {
+        if(root == NULL)
+            return 0;
 
-/* Driver program to test size function*/
+        int left = solve(root -> left);
+        int right = solve(root -> right);
 
-  
-
-int main() {
-
-   
-    int t;
-    scanf("%d ", &t);
-    while (t--) {
-        string s, ch;
-        getline(cin, s);
-        
-        Node* root = buildTree(s);
-        Solution ob;
-        cout << ob.isBalanced(root) << endl;
+        if(abs(left-right)> 1)
+            return -1;
+        if(left == -1 || right == -1)
+            return -1;
+        return max(left, right)+1;
     }
-    return 0;
-}
+    
+    bool isBalanced(TreeNode* root) 
+    {
+        if(solve(root) == -1)
+            return false;
+        else
+            return true;   
+    }
+};
 
-// } Driver Code Ends
+
