de76ee049c512fb68a68ba079a9628b803e94975
Swaraj Singh Bhati <114081769+Swaraj-singh-bhati@users.noreply.github.com>
2023-07-19 12:41:13 +0530

    Create sortedArrayToBST_LC.cpp

---
+++ sortedArrayToBST_LC.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    TreeNode* build(vector<int>& nums,int left, int right) 
+    {
+        if(left>right)
+            return NULL;
+        int mid = (left+right)/2;
+        TreeNode* NewNode = new TreeNode(nums[mid]);
+        NewNode->left = build(nums,left,mid-1);
+        NewNode->right = build(nums,mid+1,right);
+        return NewNode;
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) 
+    {
+        return build(nums,0,nums.size()-1);
+    }
+};
